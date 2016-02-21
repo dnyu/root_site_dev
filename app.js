bb0f8842e891347e423fe184078fd1555d22e60f
Michael Raevsky <raevskym@sas.upenn.edu>
2016-02-20 21:07:18 -0500

    forHighlands

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 var express = require('express');
 var app = express();
 
+$("b1").click(function() {
+    $('html,body').animate({
+        scrollTop: $("p1").offset().top},
+        'slow');
+});
+
+$("b2").click(function() {
+    $('html,body').animate({
+        scrollTop: $("p2").offset().top},
+        'slow');
+});
+
+$("b3").click(function() {
+    $('html,body').animate({
+        scrollTop: $("p3").offset().top},
+        'slow');
+});
+
+$("b4").click(function() {
+    $('html,body').animate({
+        scrollTop: $("p4").offset().top},
+        'slow');
+});
+
 app.use(express.bodyParser());
 app.use(express.logger("default"));
 
@@ -18,5 +42,5 @@ app.set('view engine', 'html');
 /* Run the server */
 
 console.log('Root');
-app.listen(8080);
-console.log('Server running on port 8080. Now open http://localhost:80/ in your browser!');
+app.listen(80);
+console.log('Server running on port 80. Now open http://localhost:80/ in your browser!');
