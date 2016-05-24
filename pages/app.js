ebab5e884fbb796be6ac792a74298095a877b870
Don Yu <dyu@seas.upenn.edu>
2016-05-23 20:45:18 -0500

    redirect

--- pages/app.js
+++ pages/app.js
@@ -10,5 +10,5 @@ angular.module('myApp', [
   'myApp.contact',
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/home'});
+  $routeProvider.otherwise({redirectTo: '/redirect'});
 }]);
